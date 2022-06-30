022d25016f3a7d74c24f512b61caeb7718305abe
icebarf <sysgrammer@protonmail.com>
2022-06-30 17:11:33 +0530

    documentation for utility.c

--- src/utility.c
+++ src/utility.c
@@ -28,9 +28,17 @@
 #include "token.h"
 #include "utility.h"
 
+/* some external globals for use */
 extern bool had_error;
 extern const char* TokenTypeString[40];
 
+/* read a file into a buffer and return it.
+ * Params:
+ * @filename : name of file to be read
+ * @filesize : a pointer to a size_t - set to the size of file read
+ * Ret:
+ * @char* : pointer to null terminated char sequence - the file read
+ */
 char* readfile(const char* filename, size_t* filesize)
 {
     /* open a file stream to filename*/
@@ -100,6 +108,15 @@ char* readfile(const char* filename, size_t* filesize)
     return filedata;
 }
 
+/* gets a heap-allocated substring from the given string
+ * Params:
+ * @str : the main string from which the substring is to be obtained
+ * @start : the offset to the first character of substring in str
+ * @end : the offset to the last character of substring in str
+ * Ret:
+ * @const char* : a pointer to a const null-terminated char array
+ * i.e the substring
+ */
 const char* get_substr(const char* str, size_t start, size_t end)
 {
     /* return null if the size to get is zero */
@@ -117,17 +134,35 @@ const char* get_substr(const char* str, size_t start, size_t end)
     return substr;
 }
 
-void report(int line, const char* where, const char* message)
+/* report an error to stderr -- internal use only
+ * Params:
+ * @line : the line where the error occurred
+ * @where : the string specifying where the error occurred
+ * @message : the error message
+ */
+static void report(int line, const char* where, const char* message)
 {
     fprintf(stderr, "[line %d ] Error %s : %s\n", line, where, message);
 }
 
+/* print out an error
+ * Params:
+ * @line : the line where the error occurred
+ * @message : the error message
+ */
 void error(int line, const char* message)
 {
     report(line, "", message);
     had_error = true;
 }
 
+/* non-lexicographic string compare
+ * compares two strings upto count characters or
+ * until either of two strings has a zero-byte
+ * Params:
+ * @s1 : the first string
+ * @s2 : the second string
+ * @count : number of characters to compare */
 bool strncmp_nl(const char* s1, const char* s2, size_t count)
 {
     if (s1 == NULL || s2 == NULL)
@@ -144,6 +179,14 @@ bool strncmp_nl(const char* s1, const char* s2, size_t count)
     return true;
 }
 
+/* get the token type of a keyword from a given string if it matches
+ * Params:
+ * @str: the string which is to be compared and the
+ * keyword key is to be obtained
+ *Ret:
+ * @enum TOKEN_TYPE: a token type is returned for iff a string match
+ * is found with a keyword
+ */
 enum TOKEN_TYPE get_keyword(const char* str)
 {
     if (strncmp_nl(str, TokenTypeString[AND], strlen(TokenTypeString[AND])))
